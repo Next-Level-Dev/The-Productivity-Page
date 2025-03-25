6d733925e420ffb47a886ae3875dc229c68de584
Next-Level-Dev <shadowdiary01@gmail.com>
2025-03-25 20:31:24 +0300

    big update

---
+++ click_counter/js.js
@@ -0,0 +1,8 @@
+let count = 0;
+const countDisplay = document.getElementById('count');
+const clickButton = document.getElementById('clickButton');
+
+clickButton.addEventListener('click', () => {
+    count++;
+    countDisplay.textContent = count;
+});
